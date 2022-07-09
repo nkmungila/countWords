ca03d1bbe3017651d0c4da41dd3ac3b61ddca322
Naveena Krishna <nkmungila@gmail.com>
2022-07-10 00:05:19 +0530

    Search for lowercase too

--- WordOccurence.js
+++ WordOccurence.js
@@ -1,41 +1,39 @@
 // This program search the given word in the given file and giving you how many times
-// the given word appears in the file. It will search in exact case.
+// the given word appears in the file. 
 const {readFileSync} = require('fs');
-
-
-
 const run = () =>{
     const fileName = 'sample.txt'
     const findword = 'one'
     const filecontent = syncReadFile(fileName);
     const wordsInFile = getWords(filecontent)
     const occurence = occurenceInFile(wordsInFile,findword)
-    console.log("In the given file the word "+findword+' occured '+occurence+" times. ")
+    console.log(`In the given file the word ${findword} occured ${occurence} times.`)
 }
 
 
-function syncReadFile(filename) {
+const syncReadFile = (filename)=>{
   try{
         const contents = readFileSync(filename, 'utf-8');
         return contents;
-        }   catch(e){
-            console.log('File Not Found')
-           }
+    } catch(e){
+        console.log('File Not Found')
+    }
 }
 
-function getWords(text){
-    let x = text.replace(/[^A-Za-z0-9]+/g, " ");
-    let newArr = x.trim().split(" ");
+const getWords = (text) =>{
+    const w = text.toLowerCase();
+    const x = w.replace(/[^A-Za-z0-9]+/g, " ");
+    const newArr = x.trim().split(" ");
     return newArr;
 }
 
-function occurenceInFile(listOfWord,findWord){
+const occurenceInFile = (listOfWord,findWord) =>{
     let count = 0;
     listOfWord.forEach(element => {
         if (element === findWord) {
-        count += 1;
+            count += 1;
         }
-})
+    })
     return count
 }
 
